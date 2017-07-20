2683a19d88b3b1b30d58e7ab0cd60d57b1790b37
Brad Larson <contact@sunsetlakesoftware.com>
2017-07-20 18:32:18 -0500

    Experimental addition of a synchronous image read from PictureOutput.

--- framework/Source/iOS/PictureOutput.swift
+++ framework/Source/iOS/PictureOutput.swift
@@ -11,6 +11,8 @@ public class PictureOutput: ImageConsumer {
     public var encodedImageFormat:PictureFileFormat = .png
     public var imageAvailableCallback:((UIImage) -> ())?
     public var onlyCaptureNextFrame:Bool = true
+    public var keepImageAroundForSynchronousCapture:Bool = false
+    var storedFramebuffer:Framebuffer?
     
     public let sources = SourceContainer()
     public let maximumInputs:UInt = 1
@@ -55,6 +57,11 @@ public class PictureOutput: ImageConsumer {
     }
     
     public func newFramebufferAvailable(_ framebuffer:Framebuffer, fromSourceIndex:UInt) {
+        if keepImageAroundForSynchronousCapture {
+            storedFramebuffer?.unlock()
+            storedFramebuffer = framebuffer
+        }
+        
         if let imageCallback = imageAvailableCallback {
             let cgImageFromBytes = cgImageFromFramebuffer(framebuffer)
             
@@ -83,6 +90,18 @@ public class PictureOutput: ImageConsumer {
                 encodedImageAvailableCallback = nil
             }
         }
+    }
+    
+    public func synchronousImageCapture() -> UIImage {
+        var outputImage:UIImage!
+        sharedImageProcessingContext.runOperationSynchronously{
+            guard let currentFramebuffer = storedFramebuffer else { fatalError("Synchronous access requires keepImageAroundForSynchronousCapture to be set to true") }
+            
+            let cgImageFromBytes = cgImageFromFramebuffer(currentFramebuffer)
+            outputImage = UIImage(cgImage:cgImageFromBytes, scale:1.0, orientation:.up)
+        }
+        
+        return outputImage
     }
 }
 
